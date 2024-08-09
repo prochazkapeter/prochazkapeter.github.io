b94d41375bdb0021d6331cba331ca50680b4c2e9
Petr Prochazka <prochazkapetr2@gmail.com>
2024-08-09 12:54:22 +0200

    minor fixes

--- scripts.js
+++ scripts.js
@@ -3,36 +3,36 @@ var textWrapper = document.querySelector('.ml11 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
 anime.timeline()
-.add({
-    targets: '.ml11 .line',
-    scaleY: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 700
-})
-.add({
-    targets: '.ml11 .line',
-    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 30],
-    easing: "easeOutExpo",
-    duration: 700,
-    delay: 100
-}).add({
-    targets: '.ml11 .letter',
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=775',
-    delay: (el, i) => 34 * (i+1)
-}).add({
-    targets: '.ml11',
-    opacity: 1,
-    duration: 100,
-    easing: "easeOutExpo",
-    delay: 100
-  }).add({
-    targets: '.ml11 .line',
-    opacity: [0.5,0],
-    translateX: document.querySelector('.ml11 .letters').getBoundingClientRect().width + 30,
-    easing: "easeOutExpo",
-    duration: 700
-});
+    .add({
+        targets: '.ml11 .line',
+        scaleY: [0, 1],
+        opacity: [0.5, 1],
+        easing: "easeOutExpo",
+        duration: 700
+    })
+    .add({
+        targets: '.ml11 .line',
+        translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 30],
+        easing: "easeOutExpo",
+        duration: 700,
+        delay: 100
+    }).add({
+        targets: '.ml11 .letter',
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=775',
+        delay: (el, i) => 34 * (i + 1)
+    }).add({
+        targets: '.ml11',
+        opacity: 1,
+        duration: 100,
+        easing: "easeOutExpo",
+        delay: 100
+    }).add({
+        targets: '.ml11 .line',
+        opacity: [0.5, 0],
+        translateX: document.querySelector('.ml11 .letters').getBoundingClientRect().width + 30,
+        easing: "easeOutExpo",
+        duration: 700
+    });
